189a65df6d53d73e7b9c86d7bd2ab586c8e636a9
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-07-12 23:17:26 +0900

    Listing In-App Purchases

--- Rage/IAPHelper.swift
+++ Rage/IAPHelper.swift
@@ -28,8 +28,13 @@ public typealias ProductsRequestCompletionHandler = (_ success: Bool, _ products
 open class IAPHelper : NSObject  {
   
   static let IAPHelperPurchaseNotification = "IAPHelperPurchaseNotification"
+  fileprivate let productIdentifiers: Set<ProductIdentifier>
+  fileprivate var purchasedProductIdentifiers = Set<ProductIdentifier>()
+  fileprivate var productsRequest: SKProductsRequest?
+  fileprivate var productsRequestCompletionHandler: ProductsRequestCompletionHandler?
   
   public init(productIds: Set<ProductIdentifier>) {
+    productIdentifiers = productIds
     super.init()
   }
 }
@@ -38,8 +43,13 @@ open class IAPHelper : NSObject  {
 
 extension IAPHelper {
   
-  public func requestProducts(_ completionHandler: ProductsRequestCompletionHandler) {
-    completionHandler(false, [])
+  public func requestProducts(_ completionHandler: @escaping ProductsRequestCompletionHandler) {
+    productsRequest?.cancel()
+    productsRequestCompletionHandler = completionHandler
+    
+    productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
+    productsRequest!.delegate = self
+    productsRequest!.start()
   }
 
   public func buyProduct(_ product: SKProduct) {
@@ -54,5 +64,33 @@ extension IAPHelper {
   }
   
   public func restorePurchases() {
+  }
+}
+
+// MARK: - SKProductsRequestDelegate
+
+extension IAPHelper: SKProductsRequestDelegate {
+  
+  public func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+    print("Loaded list of products...")
+    let products = response.products
+    productsRequestCompletionHandler?(true, products)
+    clearRequestAndHandler()
+    
+    for p in products {
+      print("Found product: \(p.productIdentifier) \(p.localizedTitle) \(p.price.floatValue)")
+    }
+  }
+  
+  public func request(_ request: SKRequest, didFailWithError error: Error) {
+    print("Failed to load list of products.")
+    print("Error: \(error.localizedDescription)")
+    productsRequestCompletionHandler?(false, nil)
+    clearRequestAndHandler()
+  }
+  
+  private func clearRequestAndHandler() {
+    productsRequest = nil
+    productsRequestCompletionHandler = nil
   }
 }
